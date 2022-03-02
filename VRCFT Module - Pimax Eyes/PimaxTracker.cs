3a16daaa7c0b0aee28a359ee8aec0317cefe2fd1
Dazbme <jlehmann96@gmail.com>
2022-03-02 02:47:42 -0600

    Adding Pimax eye tracking module - Eye position works but openness does not

---
+++ VRCFT Module - Pimax Eyes/PimaxTracker.cs
@@ -0,0 +1,62 @@
+﻿using System.Runtime.InteropServices;
+
+namespace VRCFT_Module___Pimax_Eye_Tracking
+{
+	public class Ai1EyeData
+	{
+		public EyeExpressionState Left = PimaxTracker.GetEyeData(Eye.Left);
+		public EyeExpressionState Right = PimaxTracker.GetEyeData(Eye.Right);
+		public EyeExpressionState Recommended = PimaxTracker.GetEyeData(Eye.Any);
+
+		public void Update()
+		{
+			Left = PimaxTracker.GetEyeData(Eye.Left); 
+			Right = PimaxTracker.GetEyeData(Eye.Right);
+		    Recommended = PimaxTracker.GetEyeData(Eye.Any);
+		}
+	}
+	
+	
+	// Mad props to NGenesis for these bindings <3
+
+	public enum Eye {
+		Any,
+		Left,
+		Right
+	}
+
+	public enum CallbackType {
+		Start,
+		Stop,
+		Update
+	}
+
+	public struct EyeExpressionState
+	{
+		public float PupilCenterX;
+		public float PupilCenterY;
+		public float Openness;
+	};
+
+	public delegate void EyeTrackerEventHandler();
+	
+    public static class PimaxTracker
+    {
+	    /// <summary>
+	    /// Registers callbacks for the tracker to notify when it's finished initializing, when it has new data available and when the module is stopped.
+	    /// </summary>
+        [DllImport("PimaxEyeTracker", EntryPoint = "RegisterCallback")] public static extern void RegisterCallback(CallbackType type, EyeTrackerEventHandler callback);
+	    
+	    /// <summary>
+	    /// Initializes the module.
+	    /// </summary>
+	    /// <returns>Initialization Successful</returns>
+		[DllImport("PimaxEyeTracker", EntryPoint = "Start")] public static extern bool Start();
+	    
+	    /// <summary>
+	    /// Stops the eye tracking module and disconnects the server
+	    /// </summary>
+		[DllImport("PimaxEyeTracker", EntryPoint = "Stop")] public static extern void Stop();
+	    [DllImport("PimaxEyeTracker", EntryPoint = "GetEyeData")] public static extern EyeExpressionState GetEyeData(Eye eye);
+    }
+}
---
+++ VRCFT Module - Pimax Eyes/PimaxTrackingInterface.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Threading;
+using VRCFaceTracking;
+using VRCFaceTracking.Params;
+
+namespace VRCFT_Module___Pimax_Eye_Tracking
+{ 
+    public class PimaxTrackingInterface : ITrackingModule
+    {
+        private static readonly Ai1EyeData PimaxEyeData = new Ai1EyeData();
+        private static readonly CancellationTokenSource CancellationToken = new CancellationTokenSource();
+        
+        private static bool _needsUpdate;
+        
+        public bool SupportsEye => true;
+        public bool SupportsLip => false;
+
+        public (bool eyeSuccess, bool lipSuccess) Initialize(bool eye, bool lip)
+        {
+            PimaxTracker.RegisterCallback(CallbackType.Update, () => _needsUpdate = true);
+
+            var success = PimaxTracker.Start();
+            return (success, false);
+        }
+
+        public Action GetUpdateThreadFunc()
+        {
+            return () =>
+            {
+                while (!CancellationToken.IsCancellationRequested)
+                {
+                    if (_needsUpdate)
+                        Update();
+
+                    Thread.Sleep(10);
+                }
+            };
+        }
+
+        public void Teardown()
+        {
+            CancellationToken.Cancel();
+            PimaxTracker.Stop();
+        }
+
+        public void Update()
+        {
+            if (!UnifiedLibManager.EyeEnabled) return;
+            
+            PimaxEyeData.Update();
+            //UnifiedTrackingData.LatestEyeData.UpdateData(PimaxEyeData);
+            UnifiedTrackingData.LatestEyeData.Left.Look = new Vector2((PimaxEyeData.Left.PupilCenterX * 2 - 1)*2, (PimaxEyeData.Left.PupilCenterY * 2 - 1)*2);
+            UnifiedTrackingData.LatestEyeData.Right.Look = new Vector2((PimaxEyeData.Right.PupilCenterX * 2 - 1)*2, (PimaxEyeData.Right.PupilCenterY * 2 - 1)*2);
+            UnifiedTrackingData.LatestEyeData.Combined.Look = new Vector2((PimaxEyeData.Recommended.PupilCenterX * 2 - 1)*2, (PimaxEyeData.Recommended.PupilCenterY * 2 - 1)*2);
+            UnifiedTrackingData.LatestEyeData.Left.Openness = 1f; //PimaxEyeData.Left.Openness * 0.01f;
+            UnifiedTrackingData.LatestEyeData.Right.Openness = 1f; //PimaxEyeData.Right.Openness * 0.01f;
+            UnifiedTrackingData.LatestEyeData.Combined.Openness = 1f; // PimaxEyeData.Recommended.Openness * 0.01f;
+        }
+    }
+}
